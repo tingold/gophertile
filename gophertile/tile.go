1e07084777a40b742cb65594cf2227ec6cdc8e94
Tom Ingold <tingold@Toms-MBP.fios-router.home>
2017-08-28 18:45:19 -0400

    fixes to tests and deg/rad handlin

--- gophertile/tile.go
+++ gophertile/tile.go
@@ -2,102 +2,112 @@ package gophertile
 
 import (
 	"math"
-
 )
 
 const threeSixty float64 = 360.0
 const oneEighty float64 = 180.0
+const radius float64 = 6378137.0
 
 type Tile struct {
-
-	X,Y,Z int
-
+	X, Y, Z int
 }
-type LngLat struct{
-
+type LngLat struct {
 	Lng, Lat float64
 }
 
 //LngLat bounding box of a tile
-type LngLatBbox struct{
-	West,South,East,North float64
+type LngLatBbox struct {
+	West, South, East, North float64
 }
-//Spherical Mercator bounding box of a tile
-type Bbox struct{
 
+//Spherical Mercator bounding box of a tile
+type Bbox struct {
 	Left, Bottom, Right, Top float64
 }
-
-func deg2rad(deg float64) (float64){
-	return deg * oneEighty / math.Pi
-}
-func rad2deg(rad float64) (float64){
-	return rad / oneEighty * math.Pi
+type XY struct {
+	X, Y float64
 }
 
-func GetTile(lng float64, lat float64, zoom int) (*Tile){
+func deg2rad(deg float64) float64 {
+	return deg * (math.Pi / oneEighty)
+}
+func rad2deg(rad float64) float64 {
+	return rad * (oneEighty / math.Pi)
+}
 
-	lat_rad := rad2deg(lat)
-	n := math.Pow(2.0,float64(zoom))
+func GetTile(lng float64, lat float64, zoom int) *Tile {
+
+	lat_rad := deg2rad(lat)
+	n := math.Pow(2.0, float64(zoom))
 	x := int(math.Floor((lng + oneEighty) / threeSixty * n))
-	y := int(math.Floor((1.0 - math.Log(math.Tan(lat_rad) + (1.0 / math.Cos(lat_rad))) / math.Pi) /2.0 * n))
+	y := int(math.Floor((1.0 - math.Log(math.Tan(lat_rad)+(1.0/math.Cos(lat_rad)))/math.Pi) / 2.0 * n))
 
-	return &Tile{x,y,zoom}
+	return &Tile{x, y, zoom}
 
 }
 
-func (tile *Tile) Equals(t2 *Tile) (bool) {
+func (tile *Tile) Equals(t2 *Tile) bool {
 
 	return tile.X == t2.X && tile.Y == t2.Y && tile.Z == t2.Z
 
 }
 
 //Ul returns the upper left corner of the tile decimal degrees
-func (tile *Tile) Ul() (*LngLat){
+func (tile *Tile) Ul() *LngLat {
 
 	n := math.Pow(2.0, float64(tile.Z))
-	lon_deg := float64(tile.X) / n * threeSixty - oneEighty
-	lat_rad := math.Atan(math.Sinh(math.Pi * float64(1 - 2 * float64(tile.Y) / n)))
-	lat_deg := deg2rad(lat_rad)
+	lon_deg := float64(tile.X)/n*threeSixty - oneEighty
+	lat_rad := math.Atan(math.Sinh(math.Pi * float64(1-(2*float64(tile.Y)/n))))
+	lat_deg := rad2deg(lat_rad)
 
-	return &LngLat{lon_deg,lat_deg}
+	return &LngLat{lon_deg, lat_deg}
 }
 
-func (tile *Tile) Bounds() (*LngLatBbox) {
+func (tile *Tile) Bounds() *LngLatBbox {
 	a := tile.Ul()
-	shifted := Tile{tile.X+1,tile.Y+1, tile.Z}
+	shifted := Tile{tile.X + 1, tile.Y + 1, tile.Z}
 	b := shifted.Ul()
 	return &LngLatBbox{a.Lng, b.Lat, b.Lng, a.Lat}
 }
 
-func (tile *Tile) Parent()(*Tile){
+func (tile *Tile) Parent() *Tile {
 
-	if tile.Z == 0  && tile.X == 0 && tile.Y == 0 {
-		return  tile
+	if tile.Z == 0 && tile.X == 0 && tile.Y == 0 {
+		return tile
 	}
 
-	if math.Mod(float64(tile.X) , 2) == 0 && math.Mod(float64(tile.Y), 2) == 0{
-		return &Tile{tile.X / 2,tile.Y / 2,tile.Z -1}
+	if math.Mod(float64(tile.X), 2) == 0 && math.Mod(float64(tile.Y), 2) == 0 {
+		return &Tile{tile.X / 2, tile.Y / 2, tile.Z - 1}
 	}
-	if math.Mod(float64(tile.X),2) == 0 {
-		return &Tile{tile.X / 2,(tile.Y-1) / 2,tile.Z -1}
+	if math.Mod(float64(tile.X), 2) == 0 {
+		return &Tile{tile.X / 2, (tile.Y - 1) / 2, tile.Z - 1}
 	}
-	if math.Mod(float64(tile.X) , 2) != 0 && math.Mod(float64(tile.Y), 2) != 0{
-		return &Tile{(tile.X-1) / 2,(tile.Y-1) / 2,tile.Z -1}
+	if math.Mod(float64(tile.X), 2) != 0 && math.Mod(float64(tile.Y), 2) != 0 {
+		return &Tile{(tile.X - 1) / 2, (tile.Y - 1) / 2, tile.Z - 1}
 	}
-	if math.Mod(float64(tile.X) , 2) != 0 && math.Mod(float64(tile.Y), 2) == 0{
-		return &Tile{(tile.X-1) / 2,tile.Y / 2,tile.Z -1}
+	if math.Mod(float64(tile.X), 2) != 0 && math.Mod(float64(tile.Y), 2) == 0 {
+		return &Tile{(tile.X - 1) / 2, tile.Y / 2, tile.Z - 1}
 	}
 	return nil
 }
 
-func (tile *Tile) Children()([]*Tile){
+func (tile *Tile) Children() []*Tile {
 
 	kids := []*Tile{
 		&Tile{tile.X * 2, tile.Y * 2, tile.Z + 1},
-		&Tile{tile.X * 2 + 1, tile.Y * 2, tile.Z + 1},
-		&Tile{tile.X * 2 + 1, tile.Y * 2 + 1, tile.Z + 1},
-		&Tile{tile.X * 2, tile.Y * 2 + 1, tile.Z + 1},
+		&Tile{tile.X*2 + 1, tile.Y * 2, tile.Z + 1},
+		&Tile{tile.X*2 + 1, tile.Y*2 + 1, tile.Z + 1},
+		&Tile{tile.X * 2, tile.Y*2 + 1, tile.Z + 1},
 	}
 	return kids
+}
+
+//XY transforms WGS84 DD to Spherical Mercator meters
+func ToXY(ll *LngLat) *XY {
+
+	x := radius * deg2rad(ll.Lng)
+	intrx := (math.Pi * 0.25) + (0.5 * deg2rad(ll.Lat))
+	y := radius * math.Log(math.Tan(intrx))
+
+	return &XY{x, y}
 }
--- gophertile/tile_test.go
+++ gophertile/tile_test.go
@@ -1,74 +1,101 @@
 package gophertile
 
 import (
-	"testing"
 	"fmt"
+	"math"
+	"testing"
 )
 
 func TestTile_Ul(t *testing.T) {
 
-	tile := Tile{486,332,10,}
+	tile := Tile{486, 332, 10}
 	ll := tile.Ul()
 	expected := LngLat{-9.140625, 53.33087298301705}
 
-	assertEq(t,ll.Lng,expected.Lng)
-	assertEq(t,ll.Lat,expected.Lat)
+	assertPrettyClose(t, ll.Lng, expected.Lng)
+	assertPrettyClose(t, ll.Lat, expected.Lat)
 
 }
 
 func TestTile_Bounds(t *testing.T) {
 
-	tile := Tile{486,332,10,}
+	tile := Tile{486, 332, 10}
 	expected := LngLatBbox{-9.140625, 53.120405283106564, -8.7890625, 53.33087298301705}
 	bbox := tile.Bounds()
-	assertEq(t, expected.East, bbox.East)
-	assertEq(t, expected.West, bbox.West)
-	assertEq(t, expected.North, bbox.North)
-	assertEq(t, expected.South, bbox.South)
+	assertPrettyClose(t, expected.East, bbox.East)
+	assertPrettyClose(t, expected.West, bbox.West)
+	assertPrettyClose(t, expected.North, bbox.North)
+	assertPrettyClose(t, expected.South, bbox.South)
 }
 
 func TestTile_Parent(t *testing.T) {
 	expected := Tile{243, 166, 9}
 	tile := Tile{486, 332, 10}
 	parent := tile.Parent()
-	assertEq(t, expected.X,parent.X)
-	assertEq(t, expected.Y,parent.Y)
-	assertEq(t, expected.Z,parent.Z)
+	assertEq(t, expected.X, parent.X)
+	assertEq(t, expected.Y, parent.Y)
+	assertEq(t, expected.Z, parent.Z)
 }
 
 func TestTile_Children(t *testing.T) {
 
-	tile := Tile{246,166,9}
+	tile := Tile{246, 166, 9}
 	expected := Tile{492, 332, 10}
 	children := tile.Children()
 
 	found := false
-	for _,t2 := range children {
-		if t2.Equals(&expected){
+	for _, t2 := range children {
+		if t2.Equals(&expected) {
 			found = true
 		}
 	}
-	if !found{
+	if !found {
 		t.Fail()
 	}
+
+}
+
+func TestToXY(t *testing.T) {
+
+	expected := XY{-1017529.7205322663, 7044436.526761846}
+	tile := Tile{486, 332, 10}
+	//expected := XY{-0.0,0.0}
+
+	ll := tile.Ul()
+	//xy := ToXY(&LngLat{0.0,0.0})
+	xy := ToXY(ll)
+
+	assertPrettyClose(t, xy.Y, expected.Y)
+	assertPrettyClose(t, xy.X, expected.X)
 
 }
 
 func TestGetTile(t *testing.T) {
 
 	tile := GetTile(20.6852, 40.1222, 9)
-	expected := Tile{285,193,9}
+	expected := Tile{285, 193, 9}
 
-	assertEq(t,tile.Z, expected.Z)
-	assertEq(t,tile.Y, expected.Y)
-	assertEq(t,tile.X, expected.X)
+	assertEq(t, tile.Z, expected.Z)
+	assertEq(t, tile.Y, expected.Y)
+	assertEq(t, tile.X, expected.X)
 
 }
 
-func assertEq(t *testing.T, x interface{}, y interface{}){
+func assertPrettyClose(t *testing.T, x float64, y float64) {
+
+	if x != y {
+		if math.Abs(x-y) < 0.00000001 {
+			//floats and shit
+			return
+		}
+		t.Fail()
+	}
+}
+
+func assertEq(t *testing.T, x interface{}, y interface{}) {
 	if x != y {
 
-		fmt.Printf("%v is not equal to %v", x , y)
+		fmt.Printf("%v is not equal to %v", x, y)
 		t.Fail()
 	}
 }
